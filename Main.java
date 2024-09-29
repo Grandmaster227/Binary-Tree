ec6e1ec94cd9d17877aff9174ab53aeb5403afce
Grandmaster <124741169+Grandmaster227@users.noreply.github.com>
2024-09-29 01:24:55 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,18 +2,30 @@ public class Main {
     public static void main(String[] args) {
 
         Node root = new Node(10);
-        root.left = new Node(11);
-        root.right = new Node(1204);
-        root.left.left = new Node(222);
-        root.left.right = new Node(1240);
-        root.right.left = new Node(1102);
-        root.right.right = new Node(17);
+        root.add(11);
+        root.add(213);
+        root.add(1204);
+        root.add(16);
+        root.add(1102);
+        root.add(333);
+
 
         System.out.println(Bst.pesquisar(root, 29) != null ? "Found" : "Not Found");
 
         System.out.println(Bst.pesquisar(root, 22) != null ? "Found" : "Not Found");
 
+        System.out.println("\nÁrvore inicial:");
         Node.treePrint(root);
+
+        root = Node.removeNode(root, 213);
+        System.out.println("\nÁrvore depois de remover o valor 213:");
+        Node.treePrint(root);
+
+        root = Node.removeNode(root, 16);
+        System.out.println("\nÁrvore depois de remover o valor 16:");
+        Node.treePrint(root);
+
+
         System.out.println("Hello world!");
     }
 }
--- Node.java
+++ Node.java
@@ -8,14 +8,14 @@ public class Node {
         left = null;
     }
 
+
     private Node addRecursiva(Node atual, int valor) {
         if (atual == null) {
             return new Node(valor);
         }
         if (valor < atual.k) {
             atual.left = addRecursiva(atual.left, valor);
-        }
-        else if (valor > atual.k) {
+        } else if (valor > atual.k) {
             atual.right = addRecursiva(atual.right, valor);
         }
         return atual;
@@ -25,9 +25,11 @@ public class Node {
         addRecursiva(this, valor);
     }
 
+
     public static int getcol(int height) {
-        return(int) Math.pow(2, height) - 1;
+        return (int) Math.pow(2, height) - 1;
     }
+
 
     public static int height(Node root) {
         if (root == null)
@@ -35,18 +37,29 @@ public class Node {
         return 1 + Math.max(height(root.left), height(root.right));
     }
 
+
     public void printarTree(int[][] M, Node root, int col, int row, int height) {
-            if (root == null)
-                return;
-            M[row][col] = root.k;
-            printarTree(M, root.left, col - (int)Math.pow(2, height - 2), row + 1, height - 1);
-            printarTree(M, root.right, col + (int)Math.pow(2, height - 2), row + 1, height - 1);
+        if (root == null)
+            return;
+        M[row][col] = root.k;
+        printarTree(M, root.left, col - (int) Math.pow(2, height - 2), row + 1, height - 1);
+        printarTree(M, root.right, col + (int) Math.pow(2, height - 2), row + 1, height - 1);
     }
+
+
+    public static Node findMin(Node node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
 
     public static void treePrint(Node root) {
         int h = height(root);
         int col = getcol(h);
         int[][] M = new int[h][col];
+
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < col; j++) {
@@ -54,17 +67,51 @@ public class Node {
             }
         }
 
+
         root.printarTree(M, root, col / 2, 0, h);
 
+
         for (int i = 0; i < h; i++) {
+            int espacamento = (int) Math.pow(2, h - i -1);
             for (int j = 0; j < col; j++) {
                 if (M[i][j] == 0)
-                    System.out.print("   ");
+                    System.out.print(" ".repeat(espacamento));
                 else
-                    System.out.print(M[i][j] + "  ");
+                    System.out.print(M[i][j] + " ".repeat(espacamento));
             }
             System.out.println();
         }
     }
 
+
+    public static Node removeNode(Node root, int valor) {
+        if (root == null) {
+            return null;
+        }
+
+
+        if (valor < root.k) {
+            root.left = removeNode(root.left, valor);
+        } else if (valor > root.k) {
+            root.right = removeNode(root.right, valor);
+        } else {
+
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            else if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+
+            Node nodeMin = findMin(root.right);
+            root.k = nodeMin.k;
+            root.right = removeNode(root.right, nodeMin.k);
+        }
+
+        return root;
+    }
 }
